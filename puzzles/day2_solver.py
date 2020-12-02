42a2a6ed61aea7744c13c5345219ede3c67d7f0e
Buote Xu <mail@buotexu.de>
2020-12-02 06:55:56 +0100

    day2, and better template

---
+++ puzzles/day2_solver.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import io
+import re
+import sys
+from loguru import logger
+import aoc2020
+
+logger.remove()
+logger.add(sys.stderr, format="{level} {name}:{function}:{line} {message}")
+
+
+
+testdata = """
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+"""
+
+def func(input):
+    data = pd.read_csv(input, sep=" ", names=["policy", "letter", "password"])
+    data.letter = data.letter.str[0]
+    logger.info(data.head())
+    def test_password(row):
+        policy = row.policy
+        letter = row.letter
+        password = row.password
+        limit_lower = int(policy.split("-")[0])
+        limit_upper = int(policy.split("-")[1])
+        count = len(re.findall(letter, password))
+        return count >= limit_lower and count <= limit_upper
+
+    result = data.apply(test_password, axis=1)
+    logger.info(result[(result == True)].count())
+
+    def test_password2(row):
+        policy = row.policy
+        letter = row.letter
+        password = row.password
+        limit_lower = int(policy.split("-")[0])
+        limit_upper = int(policy.split("-")[1])
+        return (
+            (password[limit_lower - 1] == letter and password[limit_upper - 1] != letter ) 
+         or 
+            (password[limit_lower - 1] != letter and password[limit_upper - 1] == letter ) 
+        )
+
+    result = data.apply(test_password2, axis=1)
+    logger.info(result[(result == True)].count())
+
+func(io.StringIO(testdata))
+func("./day2_input.txt")
--- puzzles/template.py
+++ puzzles/template.py
@@ -1,4 +1,22 @@
 import numpy as np
 import pandas as pd
+import io
+import re
+import sys
+from loguru import logger
 
 import aoc2020
+
+logger.remove()
+logger.add(sys.stderr, format="{level} {name}:{function}:{line} {message}")
+
+testdata = """
+"""
+
+
+def func(input):
+    data = pd.read_csv(input, sep=" ", names=[])
+
+
+func(io.StringIO(testdata))
+func("./dayx_input.txt")
