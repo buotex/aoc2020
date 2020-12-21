7b1e70fb18e40b9958ebedf8e885b7087d9e0f65
Buote Xu <mail@buotexu.de>
2020-12-21 10:41:06 +0100

    also get older versions

--- scripts/get_puzzle.sh
+++ scripts/get_puzzle.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 day=$1
-cp puzzles/template.py puzzles/day${day}_solver.py
-sed -i "s/dayx_input/day${day}_input/g" puzzles/day${day}_solver.py
-wslview https://adventofcode.com/2020/day/${day}
-python -c "from aocd.models import Puzzle; print(Puzzle(year=2020, day=${day}).input_data)" > puzzles/day${day}_input.txt
+
+if [[ -z "$2" ]]; then
+    year=2020
+else
+    year = $2
+fi
+cp $year/puzzles/template.py puzzles/$year/day${day}_solver.py
+sed -i "s/dayx_input/day${day}_input/g" puzzles/$year/day${day}_solver.py
+wslview https://adventofcode.com/$year/day/${day}
+python -c "from aocd.models import Puzzle; print(Puzzle(year=$year, day=${day}).input_data)" > puzzles/$year/day${day}_input.txt
 
