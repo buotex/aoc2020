aa395a9896d61160b5b8f28083e61c06ae27636f
Buote Xu <mail@buotexu.de>
2020-12-22 08:36:33 +0100

    multi-year

--- scripts/get_puzzle.sh
+++ scripts/get_puzzle.sh
@@ -5,9 +5,9 @@ day=$1
 if [[ -z "$2" ]]; then
     year=2020
 else
-    year = $2
+    year=$2
 fi
-cp $year/puzzles/template.py puzzles/$year/day${day}_solver.py
+cp puzzles/$year/template.py puzzles/$year/day${day}_solver.py
 sed -i "s/dayx_input/day${day}_input/g" puzzles/$year/day${day}_solver.py
 wslview https://adventofcode.com/$year/day/${day}
 python -c "from aocd.models import Puzzle; print(Puzzle(year=$year, day=${day}).input_data)" > puzzles/$year/day${day}_input.txt
